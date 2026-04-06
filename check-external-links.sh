1e7e9f6f41dc6e98b435bd280c57c0ba5a61e793
Dean Wampler <dean.wampler@ibm.com>
2026-04-06 14:44:41 -0400

    Updated external link checker.
    Signed-off-by: Dean Wampler <dean.wampler@ibm.com>

--- check-external-links.sh
+++ check-external-links.sh
@@ -13,9 +13,10 @@ For markdown files, it prints anchor tags ("[name](url)") that don't
 have a "{:target=...}" appended to them. For HTML files, it looks for 
 the equivalent "<a href="..." target="...">...</a>".
 
-It also handles our way of referencing the separate glossary site, where
-the markdown URL will be '[Term]({{site.glossaryurl}}/#term)' and not have
-an explicit 'http...' leader. 
+It also handles two '{{site.*url}}' definitions we use for referencing 
+commonly used external sites:
+* '{{site.glossaryurl}}' for the separate glossary site.
+* '{{site.gh_edit_repository}}' for the GitHub repo URL.
 
 It attempts to correctly ignore image URLs, e.g., '![label](https://example.com/image.png)',
 by looking at the file extension (jpg|jpeg|png|svg|mp3|mp4). This means
@@ -95,6 +96,11 @@ do
 		--exclude-dir '_site' --exclude-dir '_sass' \
 		$path | $eg -v 'target=' | $eg -v '\.(jpg|jpeg|png|svg|mp3|mp4)'
 	$NOOP $eg -nHoR '\(\{\{site.glossaryurl\}\}[^)]*\)(\S*)' \
+		--include '*.markdown' --include '*.md' \
+ 		--exclude-dir 'temp' --exclude-dir 'tmp' \
+		--exclude-dir '_site' --exclude-dir '_sass' \
+		$path | $eg -v 'target=' | $eg -v '\.(jpg|jpeg|png|svg|mp3|mp4)'
+	$NOOP $eg -nHoR '\(\{\{site.gh_edit_repository\}\}[^)]*\)(\S*)' \
 		--include '*.markdown' --include '*.md' \
  		--exclude-dir 'temp' --exclude-dir 'tmp' \
 		--exclude-dir '_site' --exclude-dir '_sass' \
